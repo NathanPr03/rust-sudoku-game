06e6baa177a6d022af9a11035d1316efe4284e97
Nathan <nathan.pringle@n-able.com>
2023-02-15 09:59:48 +0000

    Allocate more stack size for test

--- tests/array_matrix_test.rs
+++ tests/array_matrix_test.rs
@@ -1,25 +1,36 @@
 use std::thread::Builder;
-use rust_sudoku_game::{ArrayMatrix, EXACT_COVER_MATRIX_COLUMNS, EXACT_COVER_MATRIX_ROWS, ninebyninecovermatrix};
+use rust_sudoku_game::{ArrayMatrix, BOARD_SIZE, EXACT_COVER_MATRIX_COLUMNS, EXACT_COVER_MATRIX_ROWS, ninebyninecovermatrix};
 use crate::complete_nine_by_nine_matrix::completed_nine_by_nine_cover_matrix;
 
 mod complete_nine_by_nine_matrix;
 
 #[test]
-pub fn test_nine_by_nine_array_matrix_generated_correctly()
+pub fn test_nine_by_nine_array_matrix_generated_correctly_for_empty_board()
 {
     let builder = Builder::new()
         .name("reductor".into())
         .stack_size(32 * 1024 * 1024); // 32MB of stack space
 
     let handler = builder.spawn(|| {
-        let cover_matrix: [[u32; 324]; 729]
+        let board: [[usize; BOARD_SIZE as usize]; BOARD_SIZE as usize] =
+            [[0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0]];
+
+        let mut cover_matrix: [[u32; 324]; 729]
             = ninebyninecovermatrix::nine_by_nine_cover_matrix();
 
         let mut array_matrix = ArrayMatrix::new(cover_matrix);
-        array_matrix.generate_array_matrix();
+        array_matrix.create_sparse_matrix(&board, &mut cover_matrix);
 
         let completed_cover_matrix = completed_nine_by_nine_cover_matrix();
-        assert_eq!(array_matrix.get_cover_matrix(), &completed_cover_matrix);
+        assert_eq!(&cover_matrix, &completed_cover_matrix);
     }).unwrap();
 
     handler.join().unwrap();
