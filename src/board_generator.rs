02e84fbb4dcffb7334c25f688e1e2b8bacc2ea81
Nathan <nathan.pringle@n-able.com>
2023-03-15 17:20:04 +0000

    Generate random boards

--- src/board_generator.rs
+++ src/board_generator.rs
@@ -1,31 +1,38 @@
-use crate::{BOARD_SIZE, find_solution};
+use crate::{BOARD_SIZE, BOARD_SIZE_SQUARED, find_solution, pretty_print_board};
 use rand::Rng;
 
 #[derive(Copy, Clone)]
 pub enum GameDifficulty {
+    //These values are the number of clues that should be present in a 9x9 board
     Easy = 46,
     Medium = 32,
-    Hard = 20
+    Hard = 20,
 }
 
 pub struct BoardGenerator {
-    game_difficulty: GameDifficulty
+    game_difficulty: GameDifficulty,
+    number_of_random_nums_to_insert: usize,
 }
 
 impl BoardGenerator {
     pub fn new(game_difficulty: GameDifficulty) -> BoardGenerator
     {
         return BoardGenerator {
-            game_difficulty
-        }
+            game_difficulty,
+            number_of_random_nums_to_insert: 2, //This number seems low but it actually gives us 729 * 721 (525,609) potential boards
+        };
     }
 
     pub fn generate_random_board(
         &self,
-        sudoku_board: &mut [[usize; BOARD_SIZE as usize]; BOARD_SIZE as usize]
+        sudoku_board: &mut [[usize; BOARD_SIZE as usize]; BOARD_SIZE as usize],
     )
     {
-        for _clue in 0..self.game_difficulty as usize {
+        // This code is extremely performant :)
+        use std::time::Instant;
+        let now = Instant::now();
+
+        for i in 0..self.number_of_random_nums_to_insert as usize {
             loop {
                 let mut random_num_generator = rand::thread_rng();
 
@@ -33,11 +40,53 @@ impl BoardGenerator {
                 let random_row: usize = random_num_generator.gen_range(0..BOARD_SIZE) as usize;
                 let random_value: usize = random_num_generator.gen_range(0..BOARD_SIZE) as usize;
 
+                if sudoku_board[random_column][random_row] != 0
+                {
+                    continue;
+                }
+
                 sudoku_board[random_column][random_row] = random_value;
 
-                if find_solution(&mut sudoku_board.clone()) {
+                // If we are on the first iteration of the loop we dont have to check if the board is still solvable
+                if i == 0
+                {
                     break;
                 }
+
+                if !find_solution(sudoku_board) {
+                    sudoku_board[random_column][random_row] = 0;
+                }
+
+                break;
+
+            }
+        }
+        pretty_print_board(&sudoku_board);
+
+        self.remove_given_numbers_from_sudoku(sudoku_board);
+
+        pretty_print_board(&sudoku_board);
+        let elapsed = now.elapsed();
+        println!("Board generated in: {:.2?}", elapsed);
+    }
+
+    fn remove_given_numbers_from_sudoku(&self, sudoku_board: &mut [[usize; BOARD_SIZE as usize]; BOARD_SIZE as usize])
+    {
+        for _clue in 0..(BOARD_SIZE_SQUARED as usize - self.game_difficulty as usize) {
+            loop {
+                let mut random_num_generator = rand::thread_rng();
+
+                let random_column: usize = random_num_generator.gen_range(0..BOARD_SIZE) as usize;
+                let random_row: usize = random_num_generator.gen_range(0..BOARD_SIZE) as usize;
+
+                if sudoku_board[random_column][random_row] == 0
+                {
+                    continue;
+                }
+
+                sudoku_board[random_column][random_row] = 0;
+
+                break;
             }
         }
     }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use rust_sudoku_game::{BoardGenerator, EXACT_COVER_MATRIX_COLUMNS, find_solution, GameDifficulty};
+use rust_sudoku_game::{BOARD_SIZE, BoardGenerator, EXACT_COVER_MATRIX_COLUMNS, find_solution, GameDifficulty};
 use rust_sudoku_game::EXACT_COVER_MATRIX_ROWS;
 use std::thread::Builder;
 
@@ -14,33 +14,10 @@ fn main() {
 
     let handler = builder
         .spawn(|| {
-            // let mut board: [[usize; BOARD_SIZE as usize]; BOARD_SIZE as usize] = [
-            //     [5, 3, 0, 0, 7, 0, 0, 0, 0],
-            //     [6, 0, 0, 1, 9, 5, 0, 0, 0],
-            //     [0, 9, 8, 0, 0, 0, 0, 6, 0],
-            //     [8, 0, 0, 0, 6, 0, 0, 0, 3],
-            //     [4, 0, 0, 8, 0, 3, 0, 0, 1],
-            //     [7, 0, 0, 0, 2, 0, 0, 0, 6],
-            //     [0, 6, 0, 0, 0, 0, 2, 8, 0],
-            //     [0, 0, 0, 4, 1, 9, 0, 0, 5],
-            //     [0, 0, 0, 0, 8, 0, 0, 7, 9],
-            // ];
-
-            // let mut board: [[usize; BOARD_SIZE as usize]; BOARD_SIZE as usize] =
-            //     [[5, 3, 4, 6, 7, 8, 9, 1, 2],
-            //     [6, 7, 2, 1, 9, 5, 3, 4, 8],
-            //     [1, 9, 8, 3, 4, 2, 5, 6, 0],
-            //     [8, 5, 9, 7, 6, 1, 4, 2, 3],
-            //     [4, 2, 6, 8, 5, 3, 7, 9, 1],
-            //     [7, 1, 3, 9, 2, 4, 8, 5, 6],
-            //     [9, 6, 1, 5, 3, 7, 2, 8, 4],
-            //     [2, 8, 7, 4, 1, 9, 6, 3, 5],
-            //     [3, 4, 5, 2, 8, 6, 1, 7, 9]];
-
             let mut board = [
-                [1, 2, 3, 0, 0, 0, 0, 0, 0],
-                [4, 5, 6, 0, 0, 0, 0, 0, 0],
-                [7, 8, 0, 0, 0, 0, 0, 0, 9],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0],
                 [0, 0, 0, 0, 0, 0, 0, 0, 0],
                 [0, 0, 0, 0, 0, 0, 0, 0, 0],
                 [0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -54,12 +31,12 @@ fn main() {
             //     [2, 3, 4, 1],
             //     [4, 1, 2, 0]
             // ];
-            find_solution(&mut board);
+            // find_solution(&mut board);
 
-            // let gd = GameDifficulty::Easy;
-            // let bg = BoardGenerator::new(gd);
-            //
-            // bg.generate_random_board(&mut board);
+            let game_difficulty = GameDifficulty::Hard;
+            let board_generator = BoardGenerator::new(game_difficulty);
+
+            board_generator.generate_random_board(&mut board);
 
         })
         .unwrap();
